b1017400320e198c1e127e7d2f691152ce3f7abe
Tim Wissen <timwissennetwork@gmail.com>
2024-01-28 19:28:11 +0100

    v0.0.5 (1) redirect from homepage to '/untis' only after pressing 'ok' and not after 'cancel'

--- public/script.js
+++ public/script.js
@@ -2,7 +2,9 @@ function untis() {
     const button = document.getElementById('untis_button');
     button.addEventListener('click', function () {
         let className = prompt("Klasse eingeben (Bsp.: 'Klasse5a')");
-        window.location.href = `/untis?className=${className}`;
+        if (className != 0) {
+            window.location.href = `/untis?className=${className}`;
+        }
     });
 }
 untis()
